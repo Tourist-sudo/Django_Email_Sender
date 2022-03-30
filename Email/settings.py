ce25e2eb8b917ae49a5f95db931af0dfdeefcd69
Shivam Pathak <80026020+Tourist-sudo@users.noreply.github.com>
2022-03-30 11:56:08 +0000

    Update settings.py

--- Email/settings.py
+++ Email/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b-wrf9n!5g_j5@&-#jppm(+$#)hgn##8(1ucw^vth65fb21y1('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
